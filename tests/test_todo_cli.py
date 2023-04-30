acca07e6f2ac6047e83b36e86a1af3c9cafe99d1
Mahmoud Mosleh <mahmoudmosleh@live.com>
2023-04-30 17:43:24 +0100

    initial commit

---
+++ tests/test_todo_cli.py
@@ -0,0 +1,15 @@
+# tests/test_todo.py
+
+from typer.testing import CliRunner
+
+from todo import __app_name__, __version__, cli
+
+runner = CliRunner()
+
+def test_version():
+    result = runner.invoke(cli.app, ["--version"])
+    print(result.stdout)
+    print(f"{__app_name__} v{__version__}\n")
+    assert result.exit_code == 0
+    assert f"{__app_name__} v{__version__}" in result.stdout
+
